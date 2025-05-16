5d3190f44cecc46055a5dbeeeeb43d231028386d
Ishtiaq Robin <robin.pass36@gmail.com>
2025-05-16 22:38:35 +0600

    update the server.js with new port

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 import { createServer } from 'http';
 import next from 'next';
 
+const port = process.env.PORT || 3000; // cPanel PORT priority
 const app = next({ dev: false });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
     createServer((req, res) => {
         handle(req, res);
-    }).listen(process.env.PORT || 3000, '0.0.0.0', (err) => {
+    }).listen(port, '0.0.0.0', (err) => {
         if (err) throw err;
-        console.log('> Ready on http://0.0.0.0:' + (process.env.PORT || 3000));
+        console.log('> Ready on http://0.0.0.0:' + port);
     });
 });
